a5bb8e4e96d38f03d60bb49939f70ffb41b89d76
Tyler Sather <91901337+TyLrrrr@users.noreply.github.com>
2021-11-08 15:42:01 -0600

    Add files via upload

---
+++ private/converter/convert.js
@@ -0,0 +1,7 @@
+function GoldConverter(valNum) {
+  document.getElementById("outputSilver").innerHTML=valNum*5;
+}
+
+function SilverConverter(valNum) {
+  document.getElementById("outputGold").innerHTML=valNum/5;
+}
